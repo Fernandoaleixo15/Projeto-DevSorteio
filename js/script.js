5bd271dcd3887d851a625c4697e750ac18a59630
Fernando Aleixo <nandobeira2@gmail.com>
2026-02-11 18:58:19 -0300

    alteração javascript funcao if

--- js/script.js
+++ js/script.js
@@ -4,8 +4,13 @@ function generateNumber(){
   const min = Math.ceil( document.querySelector(".input-min").value);
   const max = Math.floor(document.querySelector(".input-max").value);
 
-  const result = Math.floor(Math.random() * (max - min + 1)) + min;
+  if(min >= max){
+    alert("O valor mínimo deve ser menor ou igual ao valor máximo.");
+  
+  }else{
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
  
     document.getElementById("result").innerText = `Número gerado: ${result}`;
+  }
 
 }
